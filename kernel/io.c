1a76f3bc2fe2c61a84a147c28a6bf87c3830eb29
Christopher Milan <chrismilan@ucla.edu>
2024-05-07 18:31:34 -0700

    serial output works

--- kernel/io.c
+++ kernel/io.c
@@ -20,7 +20,28 @@ void vga_init() {
   init = true;
 }
 
+#define COM1 0x3F8
+void serial_init() {
+  outb(COM1 + 1, 0x00); // Disable interrupts
+  outb(COM1 + 3, 0x80); // Enable DLAB
+  outb(COM1 + 0, 0x03); // Set divisor (baud rate)
+  outb(COM1 + 1, 0x00);
+  outb(COM1 + 3, 0x03); // 8 bits, one stop bit, no parity
+  outb(COM1 + 2, 0xC7); // enable and clear FIFO, interrupt on 14 bits available
+  outb(COM1 + 4, 0x0F); // setup modem control register
+  // NOTE: We skip testing via loopback
+}
 
+int is_transmit_empty() {
+  return inb(COM1 + 5) & 0x20;
+}
+ 
+void putchar(char a) {
+  while (is_transmit_empty() == 0);
+    outb(COM1, a);
+}
+
+/*
 void putchar(char c) {
   if (!init) vga_init();
   if (c == '\n') { tcol = 0; trow++; }
@@ -31,7 +52,7 @@ void putchar(char c) {
     for (u32 i = 0; i < 80; i++) tbuffer[24*80 + i] = (u16)' ' | tcolor << 8;
     trow = 24;
   }
-}
+} */
 
 void write(const char* data, u32 size) {
   for (u32 i = 0; i < size; i++) putchar(data[i]);
