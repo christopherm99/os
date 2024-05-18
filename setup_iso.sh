cd99d84ef72f4bd61bb905286a563724cee13d8e
Christopher Milan <chrismilan@ucla.edu>
2024-05-18 11:42:12 -0700

    cleanup old files and add README

--- setup_iso.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# args: kernel_file, limine.cfg, limine-datadir
-mkdir -p iso/boot/limine/
-cp -v $1 iso/boot/
-cp -v $2 $3/limine-bios.sys $3/limine-bios-cd.bin \
-      $3/limine-uefi-cd.bin iso/boot/limine/
-mkdir -p iso/EFI/BOOT/
-cp -v $3/BOOTX64.EFI iso/EFI/BOOT/
-
