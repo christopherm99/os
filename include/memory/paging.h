512fb4e4596cf2f731750f40bba7797add05a891
Christopher Milan <chrismilan@ucla.edu>
2024-05-26 16:58:33 -0700

    fixed paging?

--- include/memory/paging.h
+++ include/memory/paging.h
@@ -1,14 +1,17 @@
 #ifndef _MEMORY_PAGING_H
 #define _MEMORY_PAGING_H
 
-#define NEXT_ENTRY(Ptr) ((table_entry_s *) (((char *) Ptr) + sizeof(table_entry_s)))
+#define ADDR_MASK  0xFFFFFFFFFF000
+#define PRESENT    0x1
+#define READ_WRITE 0x2
 
+/*
 typedef struct {
   u8     xd :  1;
-  u16 _avl0 : 10;
-  u8   _res :  5;
+  u16       : 10;
+  u8        :  5;
   u64  addr : 40;
-  u8  _avl1 :  4;
+  u8        :  4;
   u8     ps :  1;
   u8      d :  1;
   u8      a :  1;
@@ -18,8 +21,7 @@ typedef struct {
   u8    r_w :  1;
   u8      p :  1;
 } table_entry_s;
-
-typedef table_entry_s *table_t;
+*/
 
 #endif
 
